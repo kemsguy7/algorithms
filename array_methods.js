4f6bac9aecf5b53a6776b6d337afe82d5f61c8a2
Matthew Idungafa <mattidungafa@gmail.com>
2023-12-25 10:23:24 +0100

    updates

--- array_methods.js
+++ array_methods.js
@@ -43,6 +43,26 @@ console.log( normArr.concat(arrLike) );  // 1,2[object Object]
 
 
 //for each
+// takes 3 params item, index, array 
 ["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
     console.log(`${item} is at index ${index} in ${array}`);
 })
+
+
+//SEARCHING IN ARRAY
+/*
+arr.includes returns a boolean if an item is found in the array or not while
+indexOf and lastIndefOf returns the index of the items found in the array
+
+*/
+
+/* FINDING AN OBJECT WITH A SPECIFIC CONDITION IN AN ARRAY OF OBJECTS*/
+let users = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Pete"},
+    {id: 3, name: "Mary"},
+    {id: 4, name: "Ada"},
+];
+
+let user = users.find(items => item.id == 1);
+alert(user.name)// John 
