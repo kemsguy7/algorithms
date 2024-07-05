15da165248bddd71e0d74249f8208b29d5d1ce60
Matthew Idungafa <mattidungafa@gmail.com>
2024-07-05 17:45:28 +0100

    added scrim algo1

---
+++ scrimba.js
@@ -0,0 +1,29 @@
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
+const nums = [1, 2, 3, 4, 5]
+// -->       [1, 4, 9, 16, 25]
+// Your code here
+const squares = nums.map(function(num) {
+    return num * num
+})
+
+// console.log(squares)
+
+
+
+/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "charlie", "danielle"]
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+const capitalized = names.map((name) => {
+    return name[0].toUpperCase() + name.slice(1)
+})
+
+// console.log(capitalized)
